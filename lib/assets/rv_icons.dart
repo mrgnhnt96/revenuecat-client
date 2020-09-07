7d1aae6279cc29a5258c2c1b05b95b54c9f06a33
mrgnhnt96 <mrgnhnt96@gmail.com>
2020-09-07 07:59:05 -0700

    added qr icon

---
+++ lib/assets/rv_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RVIcons {
+  RVIcons._();
+
+  static const _kFontFam = 'RVIcons';
+
+  static const IconData qr_code = IconData(0xe803, fontFamily: _kFontFam);
+}
